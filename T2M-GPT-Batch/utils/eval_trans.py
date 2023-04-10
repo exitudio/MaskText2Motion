4e9d20c72105e5c128202248150779f75deb8671
Ek Pinyoanuntapong <exitudio@gmail.com>
2023-04-10 15:01:48 -0400

    fix T2M-GPT-Batch eval predict 0 len

--- T2M-GPT-Batch/utils/eval_trans.py
+++ T2M-GPT-Batch/utils/eval_trans.py
@@ -204,10 +204,14 @@ def evaluation_transformer(out_dir, val_loader, net, trans, logger, writer, nb_i
             # [INFO] 2. this get the first index of blank_id
             pred_length = (index_motion >= blank_id).int()
             pred_length = torch.topk(pred_length, k=1, dim=1).indices.squeeze().float()
-            pred_length[pred_length==0] = index_motion.shape[1] # if blank_id in the first frame, set length to max
+            # pred_length[pred_length==0] = index_motion.shape[1] # if blank_id in the first frame, set length to max
             # [INFO] need to run single sample at a time b/c it's conv
             for k in range(bs):
-                pred_pose = net(index_motion[k:k+1, :int(pred_length[k].item())], type='decode')
+                if pred_length[k].item() > 0:
+                    motion_idx = index_motion[k:k+1, :int(pred_length[k].item())]
+                else:
+                    motion_idx = torch.ones(1,1).cuda().long()
+                pred_pose = net(motion_idx, type='decode')
                 cur_len = pred_pose.shape[1]
 
                 pred_len[k] = min(cur_len, seq)
