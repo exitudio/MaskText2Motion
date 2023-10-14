8772c57ff191a977f8233522a4312bda8b40b0ce
Ek Pinyoanuntapong <exitudio@gmail.com>
2023-10-14 12:29:39 +0000

    update train/eval config

--- T2M-BD/experiments/eval.sh
+++ T2M-BD/experiments/eval.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# cd /users/epinyoan/git/MaskText2Motion/T2M-BD/experiments/
+# cd /users/epinyoan/git/MaskText2Motion
 # sbatch eval_gpt.sh
-# cd /home/epinyoan/git/MaskText2Motion/T2M-BD/experiments/
-# screen -L -Logfile Eval_HML3D_36_token1stStage_cdim8192_32_lr0.0001_mask.5-1_predLen ~/git/MaskText2Motion/T2M-BD/experiments/eval.sh
+# cd /home/epinyoan/git/MaskText2Motion
+# screen -L -Logfile Eval_HML3D_44_crsAtt1lyr_mask0.5-1_20bRest_2 /home/epinyoan/git/MaskText2Motion/T2M-BD/experiments/eval.sh
 
 #SBATCH --job-name=eval
 #SBATCH --partition=GPU
@@ -13,20 +13,20 @@
 #SBATCH --time=47:30:00
 #SBATCH --output=%x.%j.out
 
-. ~/miniconda3/etc/profile.d/conda.sh
-cd ~/git/MaskText2Motion/T2M-BD
+. /home/epinyoan/miniconda3/etc/profile.d/conda.sh
+cd /home/epinyoan/git/MaskText2Motion/T2M-BD
 conda activate T2M-GPT
-name='Eval_HML3D_36_token1stStage_cdim8192_32_lr0.0001_mask.5-1_predLen' # TEMP
+name='Eval_HML3D_44_crsAtt1lyr_mask0.5-1_20bRest_2' # TEMP
 dataset_name='t2m'
 debug='f'
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=1
 # export CUDA_LAUNCH_BLOCKING=1
 
 python3 GPT_eval_multi.py  \
     --exp-name ${name} \
     --batch-size 128 \
     --num-layers 9 \
-    --num-local-layer 4 \
+    --num-local-layer 1 \
     --embed-dim-gpt 1024 \
     --nb-code 8192 \
     --code-dim 32 \
@@ -48,5 +48,5 @@ python3 GPT_eval_multi.py  \
     --pkeep 0.5 \
     --dilation-growth-rate 3 \
     --vq-act relu \
-    --resume-trans /home/epinyoan/git/MaskText2Motion/T2M-BD/output/t2m/2023-10-09-09-33-53_HML3D_44_crsAtt4lyr_mask0.5-1_2/net_last.pth
+    --resume-trans /home/epinyoan/git/MaskText2Motion/T2M-BD/output/t2m/2023-10-12-10-11-15_HML3D_45_crsAtt1lyr_40breset_WRONG_THIS_20BRESET/net_last.pth
 sleep 500
